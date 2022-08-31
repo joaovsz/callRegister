9b13bcc1b82accbf0d27b1b276089c45686445e6
joaovsz <joaovitor1713coin@gmail.com>
2022-08-31 14:44:37 -0300

    Layout corrigido

--- src/components/types/context.ts
+++ src/components/types/context.ts
@@ -1,12 +1,6 @@
 export type Register ={
   typeCall: string,
   typeCanceled: string, 
-  info: string
-} 
-export type DataCalls = {
-  canceladoCOMODATO: number,
-  canceladoBRI: number,
-  badCall: number,
-  retidos: number,
-  PrePago: number,
+  info: string,
+  date: Date
 } 
