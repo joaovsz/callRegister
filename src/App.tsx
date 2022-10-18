ae4c543155a5de8ababb91996aed53cf16c50920
joaovsz <joaovitor1713coin@gmail.com>
2022-10-18 09:20:47 -0300

    Roteamento adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { Header } from "./components/Header/Header"
 import '../src/components/sass/global.sass'
 import { Register } from "./components/Register/Register"
+import { Dashboard } from "./components/Dashboard/Dashboard"
+import {  Route, Routes } from "react-router-dom"
+import Login from "./components/Login/Login"
+import Signup from "./components/Signup/Signup"
 
 function App() {
 
   return (
-      <>
+    <>
       <Header />
-      <Register/>
-      </>
-    )
+      <Routes>
+        <Route element={<Login />} path="/login"/>
+        <Route element={<Signup />} path="/signup"/>
+        <Route element={<Register />} path="/"/>
+        <Route element={<Dashboard />} path="/dashboard" />
+      </Routes>
+    </>
+  )
 }
 
 export default App
