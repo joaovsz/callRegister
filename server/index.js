419ee7e23cbead011e0148bed895f65f74e08654
joaovsz <joaovitor1713coin@gmail.com>
2022-11-19 17:15:09 -0300

    cors add

--- server/index.js
+++ server/index.js
@@ -4,10 +4,6 @@ const app = express()
 const cors = require('cors')
 const userRouter = require('./routes/userRouter')
 
-header('Access-Control-Allow-Origin: *')
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS')
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token')
-
 app.use(express.json())
 app.use(cors())
 app.use('/user', userRouter)
