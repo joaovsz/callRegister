404628b78329398e88a5fc59ebfe959c9fc52419
joaovsz <joaovitor1713coin@gmail.com>
2022-11-19 17:22:25 -0300

    add cors nessa merda

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const userRouter = require('./routes/userRouter')
 
-header('Access-Control-Allow-Origin: https://callregister.netlify.app/')
+header('Access-Control-Allow-Origin: *')
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS')
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token')
 
