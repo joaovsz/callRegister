aa092719d59b90e7568ab0f5c7c5d7fa083dd713
joaovsz <joaovitor1713coin@gmail.com>
2022-11-19 16:50:20 -0300

    add Cors options

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const userRouter = require('./routes/userRouter')
-
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use('/user', userRouter)
 
 app.listen(process.env.PORT, (err, res) => {
