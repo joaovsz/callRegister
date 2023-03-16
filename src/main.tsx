b1c009a3b77972079a1394f83e4a5aea9130df25
joaovsz <joaovitor1713coin@gmail.com>
2023-03-16 11:30:24 -0300

    StrictMode removido - twice render fixed

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,11 @@ import App from './App'
 import { ChartProvider } from './components/context/chart_context'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <>
     <BrowserRouter>
       <ChartProvider>
         <App />
       </ChartProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </>
 )
